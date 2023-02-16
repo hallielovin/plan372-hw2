a15bcc20695d7cf5af0dab6dcdd206cb4c66d28e
Hallie Lovin <hallied@live.unc.edu>
2023-02-15 23:06:01 -0500

    added work for question 3

--- hw2.R
+++ hw2.R
@@ -27,7 +27,23 @@ age_score <- group_by(na.omit(data), age) %>%
 
 age_score_plot <- ggplot(age_score, aes(age, score)) + geom_point() 
 
+age_score_plot 
+
 cor(age_score$age, age_score$score)
 
 ## moderately positive correaltion -- older restaurant, higher inspection score, somewhat of a correlation but not high 
 
+#QUESTION 3
+#Look at what the different cities are to get a grasp on where misspellings exist
+unique(data$CITY)
+
+#Recode the cities that are different to look the same as their capitalized partner
+data$CITY <- recode(data$CITY, "Cary" = "CARY", "Fuquay-Varina" = "FUQUAY VARINA", "Garner" = "GARNER","Raleigh" = "RALEIGH", "Zebulon"= "ZEBULON", "Wake Forest" = "WAKE FOREST", "MORRISVILE" = "MORRISVILLE", "RTP" = "RESEARCH TRIANGLE PARK", "Apex" = "APEX", "Holly Springs" = "HOLLY SPRINGS", "Fuquay Varina" = "FUQUAY VARINA", "HOLLY SPRING" = "HOLLY SPRINGS", "Morrisville" = "MORRISVILLE" )
+
+#Check to make sure that this corrected the issues 
+unique(data$CITY)
+
+#Make a bar graph showing inspection scores and cities
+group_by(na.omit(data), CITY) %>%
+  summarize(score= mean(SCORE)) %>%
+  ggplot(aes(CITY, score)) + geom_col() + theme(axis.text.x = element_text(angle=90))
